929832e2e0868b9e27c69371ef266460c47b0721
CNBlackJ <yes.heng@icloud.com>
2018-05-31 23:00:52 +0800

    faeture: serial read

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,24 @@
 #include <Arduino.h>
 
-int START_PIN = 2;
-int END_PIN = 7;
-int index = 0;
+int LED_PIN = 13;
+int val;
 
 void setup() {
-    // put your setup code here, to run once:
-    for (int i = START_PIN; i <= END_PIN; i++) {
-			pinMode(i, OUTPUT);
-		}
+		Serial.begin(9600);
+		pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    for (int i = START_PIN; i <= END_PIN; i++) {
-			digitalWrite(i, LOW);
+		val = Serial.read(); // 读取向串口发送的数据，每次只读一个字节数据
+		if (-1 != val) {     // 没有数据到达，val为-1；有数据到达，val为数据中第一个字符的ASCII码
+		  Serial.print("val : ");
+			Serial.println(val);
+			if ('H' == val) {
+				digitalWrite(LED_PIN, HIGH);
+				delay(500);
+				digitalWrite(LED_PIN, LOW);
+				Serial.print("Available: ");
+				Serial.println(Serial.available(), DEC); // 返回串口缓冲区剩余的字符个数;最多缓冲128个字节
+			}
 		}
-    digitalWrite(START_PIN + index, HIGH);
-		index = (index + 1) % (END_PIN - START_PIN + 1);
-    delay(100);
 }
