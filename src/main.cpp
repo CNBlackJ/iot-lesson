d33cf99f5aec70d4c11af73b995c6c4d014ed710
CNBlackJ <yes.heng@icloud.com>
2018-06-07 21:21:22 +0800

    faeture: add IRremote sender & reciver

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,39 @@
 #include <Arduino.h>
+#include <IRremote.h>
 
-int latchPin = 8;
-int clockPin = 12;
-int dataPin = 11; //这里定义了那三个脚
+// int RESV_PIN = 11;
+// IRrecv irrecv(RESV_PIN);
+// decode_results results;
+
+IRsend irsend;
+
 void setup ()
 {
   Serial.begin(9600);
-  pinMode(latchPin,OUTPUT);
-  pinMode(clockPin,OUTPUT);
-  pinMode(dataPin,OUTPUT); //让三个脚都是输出状态
+  // irrecv.enableIRIn();
 }
 void loop()
 {
-  for(int a=0; a<256; a++)   //这个循环的意思是让a这个变量+1一直加到到256，每次循环都进行下面的活动
-  {
-    digitalWrite(latchPin,LOW); //将ST_CP口上面加低电平让芯片准备好接收数据
-    shiftOut(dataPin,clockPin,MSBFIRST,a);
-    //这个就是用MSBFIRST参数让0-7个针脚以高电平输出（LSBFIRST 低电平）是dataPin的参数，
-    //clockPin的参数是变量a，前面我们说了这个变量会一次从1+1+到256，是个十进制数，
-    // 输入到芯片后会产生8个二进制数，达到开关的作用
-    Serial.println(a);
-    digitalWrite(latchPin,HIGH); //将ST_CP这个针脚恢复到高电平
-    delay(1000); //暂停1秒钟让你看到效果
+  int val = Serial.read(); // 读取向串口发送的数据，每次只读一个字节数据
+  if (-1 != val) {     // 没有数据到达，val为-1；有数据到达，val为数据中第一个字符的ASCII码
+    Serial.print("val : ");
+    Serial.println(val);
+    if ('h' == val) {
+      // on: 807FC03F
+      // off: 722B04B7
+      irsend.sendNEC(0x807FC03F, 32);
+    }
+    if ('j' == val) {
+      // on: 807FC03F
+      // off: 722B04B7
+      irsend.sendNEC(0x722B04B7, 32);
+    }
   }
-  // digitalWrite(latchPin,LOW); //将ST_CP口上面加低电平让芯片准备好接收数据
-  // shiftOut(dataPin,clockPin,MSBFIRST,255);
-  // digitalWrite(latchPin,HIGH); //将ST_CP这个针脚恢复到高电平
-  // delay(500);
+
+  // if(irrecv.decode(&results)) {
+  //   Serial.println(results.value, HEX);
+  //   Serial.println();
+  //   irrecv.resume();
+  // }
+  delay(2000);
 }
