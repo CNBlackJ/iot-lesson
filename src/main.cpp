2f3e3de222554bc8d61c765522e486190cbbc83f
CNBlackJ <yes.heng@icloud.com>
2018-06-13 10:22:02 +0800

    feature: add mqtt demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,39 @@
 #include <Arduino.h>
+#include <IRremote.h>
 
-int latchPin = 8;
-int clockPin = 12;
-int dataPin = 11;
+// int RESV_PIN = 11;
+// IRrecv irrecv(RESV_PIN);
+// decode_results results;
+
+IRsend irsend;
+
 void setup ()
 {
   Serial.begin(9600);
-  pinMode(latchPin,OUTPUT);
-  pinMode(clockPin,OUTPUT);
-  pinMode(dataPin,OUTPUT);
+  // irrecv.enableIRIn();
 }
 void loop()
 {
-  int len = 8;
-  int array[len] = { 1, 2, 4, 8, 16, 32, 64, 128 };
-  for (int i = 0; i < len; i ++) {
-    Serial.println(array[i]);
-    digitalWrite(latchPin,LOW);
-    shiftOut(dataPin,clockPin,MSBFIRST,array[i]);
-    digitalWrite(latchPin,HIGH);
-    delay(500);
+  int val = Serial.read(); // 读取向串口发送的数据，每次只读一个字节数据
+  if (-1 != val) {     // 没有数据到达，val为-1；有数据到达，val为数据中第一个字符的ASCII码
+    Serial.print("val : ");
+    Serial.println(val);
+    if ('h' == val) {
+      // on: 807FC03F
+      // off: 722B04B7
+      irsend.sendNEC(0x807FC03F, 32);
+    }
+    if ('j' == val) {
+      // on: 807FC03F
+      // off: 722B04B7
+      irsend.sendNEC(0x722B04B7, 32);
+    }
   }
+
+  // if(irrecv.decode(&results)) {
+  //   Serial.println(results.value, HEX);
+  //   Serial.println();
+  //   irrecv.resume();
+  // }
+  delay(2000);
 }
