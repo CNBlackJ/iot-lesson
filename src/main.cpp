45d7b96c867b357492a51909a91fa422a7cfe3c0
CNBlackJ <yes.heng@icloud.com>
2018-07-16 09:41:59 +0800

    feat: light led

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,17 @@
 #include <Arduino.h>
-
+ 
 void setup()
 {
-   Serial.begin(9600); // 打开串口，设置波特率为9600 bps
+  pinMode(D3, OUTPUT);
+  pinMode(D4, OUTPUT);
+  Serial.begin(9600);
 }
+ 
 void loop()
 {
-   int val;
-   val=analogRead(0);   //传感器接于模拟口0
-
-   int val1;
-   val1 = digitalRead(0);
-
-   Serial.print(" A0 , D0 : ")
-   Serial.print(val)
-   Serial.print(" , ")
-   Serial.println(val1)
-   delay(100);
+  digitalWrite(D4, HIGH);
+  digitalWrite(D3, HIGH);
+  delay(500);
+  digitalWrite(D3, LOW);
+  delay(500);
 }
