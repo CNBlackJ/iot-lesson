879d4cfa54f385d0777a2aaa2564bfcac4a01bde
CNBlackJ <yes.heng@icloud.com>
2018-07-20 16:42:12 +0800

    feat: add speed lib for servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,16 @@
 #include <Arduino.h>
+#include <VarSpeedServo.h> 
+ 
+VarSpeedServo myservo;
+ 
+const int servoPin = 9;
+ 
+void setup() { 
+  myservo.attach(servoPin);
+  myservo.write(0,255,true);
+} 
 
-#define SensorLED     13
-#define SensorINPUT   3  //Connect the sensor to digital Pin 3 which is Interrupts 1.
-
-unsigned char state = 0;
-
-void blink()
-{
-  state++;
-}
-
-void setup() 
-{
-    Serial.begin(9600);
-    pinMode(SensorLED, OUTPUT);
-    pinMode(SensorINPUT, INPUT);
-    attachInterrupt(1, blink, FALLING);
-}
-void loop()
-{
-    Serial.println(state);
-    if(state!=0) {
-        state = 0;
-        digitalWrite(SensorLED, HIGH);
-        delay(500);
-    } else {
-        digitalWrite(SensorLED,LOW);
-    }
-}
+void loop() {
+  myservo.write(180,255,true);
+  myservo.write(0,30,true);
+} 
