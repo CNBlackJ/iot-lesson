434cc22c0648017e35357d123bfcccc31d27cb4c
CNBlackJ <yes.heng@icloud.com>
2018-07-27 10:13:01 +0800

    feat: add pca9685 example

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,57 @@
 #include <Arduino.h>
-#include <VarSpeedServo.h> 
- 
-VarSpeedServo myservo;
- 
-const int servoPin = 9;
- 
-void setup() { 
-  myservo.attach(servoPin);
-  myservo.write(0,255,true);
-} 
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
+
+// our servo # counter
+uint8_t servonum = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("8 channel Servo test!");
+
+  pwm.begin();
+  
+  pwm.setPWMFreq(60);  // Analog servos run at ~60 Hz updates
+
+  delay(10);
+}
+
+
+void setServoPulse(uint8_t n, double pulse) {
+  double pulselength;
+  
+  pulselength = 1000000;   // 1,000,000 us per second
+  pulselength /= 60;   // 60 Hz
+  Serial.print(pulselength); Serial.println(" us per period"); 
+  pulselength /= 4096;  // 12 bits of resolution
+  Serial.print(pulselength); Serial.println(" us per bit"); 
+  pulse *= 1000000;  // convert to us
+  pulse /= pulselength;
+  Serial.println(pulse);
+  pwm.setPWM(n, 0, pulse);
+}
 
 void loop() {
-  myservo.write(180,255,true);
-  myservo.write(0,30,true);
-} 
+  // Drive each servo one at a time
+  Serial.println(servonum);
+  for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
+    pwm.setPWM(servonum, 0, pulselen);
+    pwm.setPWM(servonum+1, 0, pulselen);
+  }
+
+  delay(500);
+  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+    pwm.setPWM(servonum, 0, pulselen);
+    pwm.setPWM(servonum+1, 0, pulselen);
+  }
+
+  delay(500);
+
+  servonum = servonum + 2;
+  if (servonum > 7) servonum = 0;
+}
