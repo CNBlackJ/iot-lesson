36a3531f779d1b140e9107fbb052f9e2e5d51737
CNBlackJ <yes.heng@icloud.com>
2018-08-20 17:07:53 +0800

    feat: add radar demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <Arduino.h>
 
+const int pinSensor = 10;
+const int pinLed = 9;
+int ismotion;
+ 
 void setup() {
-    // put your setup code here, to run once:
+  Serial.begin(9600); 
+  pinMode (pinSensor, INPUT);
+  pinMode (pinLed, OUTPUT);
 }
-
 void loop() {
-    // put your main code here, to run repeatedly:
+  ismotion = digitalRead(pinSensor);
+  Serial.println(ismotion);
+  if(ismotion == 1){                
+    digitalWrite(pinLed,HIGH);      
+    Serial.println("Detectived");    
+  }
+  else{
+    noTone(pinLed); 
+    Serial.println("Not Detectived"); 
+  }
 }
